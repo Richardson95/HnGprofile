fb83c24e020ab9055497869f016fdfc8064ec3e6
Richardson95 <134945618+Richardson95@users.noreply.github.com>
2023-09-10 10:37:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function addZero(x,n) {
+    while (x.toString().length < n) {
+      x = "0" + x;
+    }
+    return x;
+  }
+  
+  const d = new Date();
+  let h = addZero(d.getUTCHours(), 2);
+  let m = addZero(d.getUTCMinutes(), 2);
+  let s = addZero(d.getUTCSeconds(), 2);
+  let ms = addZero(d.getUTCMilliseconds(), 3);
+  let time = h + ":" + m + ":" + s + ":" + ms;
+  document.getElementById("currentUTCTime").innerHTML = time;
